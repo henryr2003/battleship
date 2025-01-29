e3605504a52342174902e900a94c194f988700aa
henryr2003 <henryrodas2003@gmail.com>
2025-01-28 23:03:58 -0800

    hopeful fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,38 @@
-const path = require('path');
+// webpack.config.js
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js', // Entry file (your main JS file)
+  mode: "development",
+  entry: "./src/index.js",
   output: {
-    filename: 'bundle.js', // Output bundle file
-    path: path.resolve(__dirname, 'dist'), // Output directory
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
   module: {
     rules: [
       {
-        test: /\.js$/, // For all .js files
-        exclude: /node_modules/, // Exclude node_modules
-        use: {
-          loader: 'babel-loader', // Use Babel to transpile
-        },
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
       },
     ],
   },
-  devServer: {
-    static: './dist', // Serve files from the 'dist' folder
-  },
-  mode: 'development', // Development mode
 };
